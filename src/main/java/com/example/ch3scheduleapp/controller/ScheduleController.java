1586aae0645927f9d7f57019860325174e1ad4f8
jy030506@naver.com <jy030506@naver.com>
2026-04-13 11:47:34 +0900

    스케쥴 생성 컨트롤러 추가

--- src/main/java/com/example/ch3scheduleapp/controller/ScheduleController.java
+++ src/main/java/com/example/ch3scheduleapp/controller/ScheduleController.java
@@ -1,7 +1,13 @@
 package com.example.ch3scheduleapp.controller;
 
+import com.example.ch3scheduleapp.dto.CreateScheduleRequest;
+import com.example.ch3scheduleapp.dto.CreateScheduleResponse;
 import com.example.ch3scheduleapp.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +16,11 @@ public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
+
+    @PostMapping("/schedules")
+    public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request){
+        CreateScheduleResponse result = scheduleService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
 
 }
